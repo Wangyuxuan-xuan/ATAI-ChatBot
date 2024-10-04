7ca7af6a69e717b425cb22b5b87065d45e7b2f13
Wangyuxuan-xuan <79263824+Wangyuxuan-xuan@users.noreply.github.com>
2024-10-04 11:10:48 +0200

    Implemented Graph loading cache

--- sparql_processor.py
+++ sparql_processor.py
@@ -1,9 +1,31 @@
 import rdflib
+import pickle
+import os
 
 class SPARQLProcessor:
-    def __init__(self, graph_path):
+
+
+    CACHE_GRAPH_PATH = './Dataset/graph.pkl'
+    
+    def __init__(self, graph_path, serialized_path=CACHE_GRAPH_PATH):
         self.graph = rdflib.Graph()
-        self.graph.parse(graph_path, format='turtle')
+        self.get_graph_cache(graph_path, serialized_path)
+
+    def get_graph_cache(self, graph_path, serialized_path):
+        # Check if serialized graph exists
+        if os.path.exists(serialized_path):
+            print("Loading serialized graph...")
+            with open(serialized_path, 'rb') as f:
+                self.graph = pickle.load(f)
+        else:
+            # Load the graph from Turtle file if serialized graph doesn't exist
+            print("Parsing Turtle file...")
+            self.graph.parse(graph_path, format='turtle')
+            
+            # Serialize the graph for future use
+            with open(serialized_path, 'wb') as f:
+                pickle.dump(self.graph, f)
+            print(f"Serialized graph saved to {serialized_path}")
 
     def process_query(self, query):
         try:
