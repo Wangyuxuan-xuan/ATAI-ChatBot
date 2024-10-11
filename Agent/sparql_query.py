b352dbd9b6be46d770bf43047a07dff025ff949c
Wangyuxuan-xuan <79263824+Wangyuxuan-xuan@users.noreply.github.com>
2024-10-11 10:28:35 +0200

    Implemented Graph Cache
    Somehow the parsing graph is quite slow on my PC, now we will cache the parsed graph into a binary file graph.pkl, and directly read it from next lunch.
    
    The file graph.pkl is untracked due to its size. Will be created locally

--- Agent/sparql_query.py
+++ Agent/sparql_query.py
@@ -1,9 +1,14 @@
+import pickle
+import os
 from rdflib import Graph
 
 class SPARQLQueryExecutor:
-    def __init__(self, dataset_path='./../Dataset/14_graph.nt'):
+    KG_GRAPH_PATH = './../Dataset/14_graph.nt'
+    CACHE_GRAPH_PATH = './../Dataset/graph.pkl'
+    
+    def __init__(self, dataset_path= KG_GRAPH_PATH):
         self.graph = Graph()
-        self.graph.parse(dataset_path, format='turtle')
+        self.get_graph_cache(dataset_path, self.CACHE_GRAPH_PATH)
 
     def execute_query(self, query):
         """Executes a SPARQL query and returns the results."""
@@ -18,3 +23,20 @@ class SPARQLQueryExecutor:
         
         return output
         
+    def get_graph_cache(self, graph_path, serialized_path):
+        # Check if serialized graph exists
+        if os.path.exists(serialized_path):
+            print("Loading serialized graph...")
+            with open(serialized_path, 'rb') as f:
+                self.graph = pickle.load(f)
+        else:
+            # Load the graph from Turtle file if serialized graph doesn't exist
+            print("Parsing KG file...")
+            self.graph.parse(graph_path, format='turtle')
+            
+            # Serialize the graph for future use
+            with open(serialized_path, 'wb') as f:
+                pickle.dump(self.graph, f)
+            print(f"Serialized graph saved to {serialized_path}")
+            # After cache 30-40 s
+            # Before cache 
